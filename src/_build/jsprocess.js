60eecedbe961103ab81e3e3daa605e269487566c
Adam Blankenship <i.adambear@gmail.com>
2022-07-16 13:30:32 -0700

    add jquery, start dev on jsprocess

---
+++ src/_build/jsprocess.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+const copyfiles = require('copyfiles');
+const eslint = require('eslint');
+const prettier = require('prettier');
+const uglify = require('uglifyjs-folder');
+
